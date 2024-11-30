a3628fbfcf59d264ce0a6b293ba318f2083fa474
sammyola246 <sammyola246@gmail.com>
2024-11-30 10:18:08 +0100

    customizations

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {copyPastePlugin} from '@superside-oss/sanity-plugin-copy-paste'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: '31e6uhqa',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), copyPastePlugin()],
 
   schema: {
     types: schemaTypes,
