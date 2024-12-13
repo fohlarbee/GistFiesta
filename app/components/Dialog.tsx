df58e32a70d6d29899f46bfb309f7d8d388e7cfe
sammyola246 <sammyola246@gmail.com>
2024-12-13 06:23:46 +0100

    news letter automation facet 1 minors 4

--- app/components/Dialog.tsx
+++ app/components/Dialog.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Button } from "@/components/ui/button"
+import { Button } from "../../components/ui/button"
 import {
   Dialog,
   DialogClose,
@@ -9,9 +9,9 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "../../components/ui/dialog"
+import { Input } from "../../components/ui/input"
+import { Label } from "../../components/ui/label"
 import { useState } from "react"
 import { useToast } from "@/hooks/use-toast"
 
